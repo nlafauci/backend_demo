14edcba66978e65c3ed5c66182cb7c2876ab4ffc
Nick Lafauci <nicholas.lafauci@gmail.com>
2023-07-29 10:02:36 -0400

    renamed index to server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config()
+
+const breadRoutes = require('./routes/bread')
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// routes
+app.use('/breads', breadRoutes)
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`listening on port ${PORT}`))
